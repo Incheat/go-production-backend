63dca1c050411b02e0207e8babd5d3b112e049e7
Incheat <incheatwork@gmail.com>
2025-12-03 17:45:52 +0900

    refactor(ping.go): add comments

--- services/helloworld/internal/handler/ping.go
+++ services/helloworld/internal/handler/ping.go
@@ -6,14 +6,20 @@ import (
 	gen "github.com/incheat/go-playground/services/helloworld/internal/api/gen/server"
 )
 
+// _ is a placeholder to ensure that Server implements the StrictServerInterface interface.
 var _ gen.StrictServerInterface = (*Server)(nil)
 
+// Server is the server for the Ping API.
+// It implements the StrictServerInterface interface.
 type Server struct{}
 
+// NewServer creates a new Server.
 func NewServer() *Server {
     return &Server{}
 }
 
+// PingV1 is the handler for the PingV1 endpoint.
+// It returns a 200 OK response with the message "pong" and the version ID "v1".
 func (s *Server) PingV1(ctx context.Context, request gen.PingV1RequestObject) (gen.PingV1ResponseObject, error) {
 	message := "pong"
 	return gen.PingV1200JSONResponse{
@@ -26,6 +32,8 @@ func (s *Server) PingV1(ctx context.Context, request gen.PingV1RequestObject) (g
 	}, nil
 }
 
+// PingV2 is the handler for the PingV2 endpoint.
+// It returns a 200 OK response with the message "pong" and the version ID "v2".
 func (s *Server) PingV2(ctx context.Context, request gen.PingV2RequestObject) (gen.PingV2ResponseObject, error) {
 	message := "pong"
 	return gen.PingV2200JSONResponse{
