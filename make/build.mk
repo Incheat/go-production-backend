091afc09e595560a33f3a2dfb5082c227868de23
Incheat <incheatwork@gmail.com>
2026-01-14 03:57:29 +0900

    ci(healthcheck): add customized probe image for http curl and grpc

--- make/build.mk
+++ make/build.mk
@@ -6,7 +6,14 @@
 ENV ?= dev
 KIND_NAME ?= dev
 
-.PHONY: build-local build-all build-all-kind
+.PHONY: build-local build-all build-all-kind build-probe
+
+build-probe: ## Build probe image
+	@echo "Building probe image"
+	docker buildx build -f infra/probe/Dockerfile \
+		-t probe:$(ENV) \
+		--platform linux/arm64 \
+		--load .
 
 build-local: ## Build one service image (SERVICE=... ENV=...) (ARM64 only)
 	@echo "Building image for $(SERVICE) (linux/arm64 ONLY)"
@@ -19,6 +26,7 @@ build-local: ## Build one service image (SERVICE=... ENV=...) (ARM64 only)
 build-all: ## Build all service images (ARM64 only)
 	@echo "Building image for all services (ARM64 ONLY)"
 	$(call for_each_service,build-local)
+	$(MAKE) build-probe
 
 build-all-kind-load: ## Build all images and load into kind (ARM64 only)
 	@echo "Building image for all services (ARM64 ONLY) and loading into kind cluster: $(KIND_NAME)"
